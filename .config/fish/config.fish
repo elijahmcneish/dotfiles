5cf307777fa88f5d4c8072b3dcc30b6525c40813
Eli McNeish <eli@mcneish.family>
2023-03-06 17:31:37 -0600

    Added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+alias ls="ls -a"
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
